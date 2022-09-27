d845f4f38992d05b7ed4a6fbbf37aec5a8b62fbc
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2022-09-27 15:36:51 +0200

    MMS-123 Set up Project Structure
    * Update .gitignore
    * Make subfolders per package. Most importantly, `core` and `allunits`
    * Minify in gulp build
    * Set up `testunits`, `testunits2` and `integrationtest` to play around with different designs. Leaving that in for now, expecting it to change or be deleted in the future.

---
+++ allunits/src/allunits.ts
@@ -0,0 +1,18 @@
+import { Decimal } from "decimal.js";
+import { config, Unit, LangString, FactorUnit } from "@qudtlib/core";
+
+export * from "@qudtlib/core";
+
+console.log("loading empty module '@qudtlib/allunits'");
+
+export const Units: any = {}
+
+export const QuantityKinds: any = {}
+
+export const Prefixes: any = {}
+
+
+
+
+
+
